7f2de11517509ed0e257bdedc91ff4d6796a8c2d
neziw <kolopajlopiotr@gmail.com>
2024-07-10 16:58:59 +0200

    Add logback configuration for logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation("com.google.code.gson:gson:2.11.0")
     implementation("redis.clients:jedis:5.2.0-beta4")
     implementation("eu.okaeri:okaeri-configs-yaml-snakeyaml:5.0.2")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
 }
 
 tasks.build {
